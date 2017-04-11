32dc211e26ab1b71a7fed3b42c3e1fdd607cb698
aldaFy <ya.dafy@yandex.ru>
2017-04-11 20:54:10 +0300

    Using lxml instead of BeautifulSoup

--- cert_enroll.py
+++ cert_enroll.py
@@ -2,14 +2,15 @@
 import argparse
 import requests
 import sys
-from BeautifulSoup import BeautifulSoup as Soup
-
+from lxml import html
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-ca', '--authority', help='Address of your MS Certification Authority', required=True)
 parser.add_argument('-r', '--request', help='Input request file', required=True)
-parser.add_argument('-c', '--certificate', help='Output certificate file', required=True)
-parser.add_argument('-p', '--proxy', help='Proxy with port without protocol, optional')
+parser.add_argument('-c', '--certificate', help='Output certificate filename', required=True)
+parser.add_argument('-p', '--proxy', help='Proxy with port without protocol')
+parser.add_argument('--chain', action='store_true', help='Get p7b chain')
+parser.add_argument('--base64', action='store_true', help='Get base64 encoded certificate or p7b chain')
 parser.add_argument('--secured', action='store_true',
                     help='Use this option if your CA is secured (using https potocol)')
 args = parser.parse_args()
@@ -19,13 +20,13 @@ try:
         request_data = req_file.read()
 except Exception as e:
     print e
-    sys.exit(-1)
+    sys.exit(1)
 
 if args.proxy:
     proxy = {'https': args.proxy} if args.secured else {'http': args.proxy}
 if requests.get('http://{}'.format(args.authority), proxies=proxy).status_code != 200:
     print "Certification Authority server is unavailable"
-    sys.exit(-1)
+    sys.exit(1)
 
 data = {'Mode': 'newreq',
         'CertRequest': request_data,
@@ -35,14 +36,22 @@ data = {'Mode': 'newreq',
         'SaveCert': 'yes'}
 
 certpage = requests.post('http://{}/certsrv/certfnsh.asp'.format(args.authority), data=data, proxies=proxy)
-soup = Soup(certpage.content)
-certificate_url = soup.findAll('a')[1]['href']
+page = html.fromstring(certpage.content)
+srv, cert_bin, cert_b64, cert_chain_bin, cert_chain_b64 = page.xpath('//a/@href')
+
+if args.chain:
+    certificate_url = cert_chain_b64 if args.base64 else cert_chain_bin
+else:
+    certificate_url = cert_b64 if args.base64 else cert_bin
+
 request = requests.get('http://{}/certsrv/{}'.format(args.authority, certificate_url), proxies=proxy)
+
+file_format = 'p7b' if args.chain else 'cer'
 
 if request.status_code == 200:
     certificate = request.content
-    with open(args.certificate) as cert:
+    with open('{}.{}'.format(args.certificate, file_format)) as cert:
         cert.write(certificate)
 else:
     print "Bad certificate url"
-    sys.exit(-1)
+    sys.exit(1)
